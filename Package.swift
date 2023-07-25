01b998d9067f6fa952798a94b7a2469f0f0951b4
Gleb Kovalenko <yourpendos@gmail.com>
2023-07-25 14:44:54 +0300

    Added package

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ServiceCore",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ServiceCore",
+            targets: ["ServiceCore"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Incetro/codex.git", from: "0.2.4"),
+        .package(url: "https://github.com/Incetro/http-transport.git", from: "5.2.8"),
+        .package(url: "https://github.com/Incetro/combine-extensions.git", .branch("main"))
+    ],
+    targets: [
+        .target(
+            name: "ServiceCore",
+            dependencies: [
+                .product(name: "CombineExtensions", package: "combine-extensions"),
+                .product(name: "Codex", package: "codex"),
+                .product(name: "HTTPTransport", package: "http-transport")
+            ]
+        ),
+        .testTarget(
+            name: "ServiceCoreTests",
+            dependencies: ["ServiceCore"]
+        ),
+    ]
+)
--- Sources/ServiceCore/Service/WebService.swift
+++ Sources/ServiceCore/Service/WebService.swift
@@ -7,6 +7,7 @@
 //
 
 import HTTPTransport
+import Foundation
 
 // MARK: - WebService
 
